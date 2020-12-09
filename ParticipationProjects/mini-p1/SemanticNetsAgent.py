2d7017e4a5a21fc6ac633c64bbad9cebcc4a8b60
tebaker <taalonn@gmail.com>
2020-12-08 21:08:17 -0800

    Added more to the mini project

--- ParticipationProjects/mini-p1/SemanticNetsAgent.py
+++ ParticipationProjects/mini-p1/SemanticNetsAgent.py
@@ -1,3 +1,18 @@
+# State will hold one instance of the game state in the form of:
+# 1) Amount of sheep, wolves on left bank
+# 2) Amount of sheep, wolves of right bank
+# 3) Current location of the boat; left bank or right bank 
+class State:
+    def __init__(self, lBank, rBank, boatLocation):
+        self.leftBank = lBank
+        self.rightBank = rBank
+        self.boatLoc = boatLocation
+    
+    def print(self):
+        print("Left Bank: ", (self.leftBank.getSheep(), self.leftBank.getWolves()))
+        print("Right Bank: ", (self.rightBank.getSheep(), self.rightBank.getWolves()))
+        print("Boat Loc: ", self.boatLoc, "\n")
+
 class Bank:
     def __init__(self, initial_sheep, initial_wolves):
         self.sheeps = initial_sheep
@@ -7,61 +22,51 @@ class Bank:
         self.sheeps = newSheep
         self.wolves = newWolves
 
+    def getSheep(self):
+        return self.sheeps
+    
+    def getWolves(self):
+        return self.wolves
+
 class SemanticNetsAgent:
     def __init__(self):
         # Holding list of tuples required to solve the problem
         self.listOfTuples = []
 
-        # Holding list of previosly explored states
-        self.previousStates = []
+        # Holding list of all previous states
+        self.listOfPreviousStates = []
 
-        # Holding current states of left, right banks for sheep, wolf information.
-        # leftBank = [[sheep, wolves]], rightBank = [[sheep, wolves]]
-        self.leftBank = Bank(0, 0)
-        self.rightBank = Bank(0, 0)
+        # If the game is still in progress, inProgress is True
+        self.inProgress = True
 
-        # Indicating if the boat is currently on the left or right bank
-        self.boatLeft = True
-
-        # Bool for if win condition is reached
-        self.stillComputing = True
-
+    # Solve will take in an initial sheep, wolves amount and run a loop while
+    # the game is still in progress
     def solve(self, initial_sheep, initial_wolves):
-        # Loading left, right banks with their animals
-        self.leftBank.setSheepWolves(initial_sheep, initial_wolves)
-        self.rightBank.setSheepWolves(0, 0)
+        # Setting initial state
+        leftBank = Bank(initial_sheep, initial_wolves)
+        rightBank = Bank(0, 0)
+        currentState = State(leftBank, rightBank, "left")
+        
+        # Adding initial state to list of previous states
+        self.listOfPreviousStates.append(currentState)
 
-        while self.stillComputing:
-            # Loading the first previous state - the initial state
-            self.previousStates.append([self.leftBank, self.rightBank])
+        while self.inProgress:
+            
+            break
 
-            # If left bank is empty, game complete. Return list of moves (tuples)
-            if self.leftBank == [0, 0]:
-                return self.listOfTuples
-            # If left bank not empty, generate list of moves to consider
-            else:
-                # If boat on left, generate possible moves for left bank to right bank
-                if self.boatLeft:
-                    self.generateAndTest(self.leftBank, self.rightBank)
-                # If boat is right, generate from right to left
-                else:
-                    self.generateAndTest(self.rightBank, self.leftBank)
+    # Generate will populate a list of all possible next-states the game
+    # progress to regardless of legality or not
+    def generate(self):        
+        pass
 
+    # Test will take in list of generated possible next-states and evaluate
+    # them for legality, discarding the illegal next-states
+    def test(self):
+        pass
 
-    def generateAndTest(self, fromBank, toBank):        
-        # Three possible ways to fill the boat:
-        # 1) 1 sheep, 1 wolf:
-        #    If sheep AND wolves greater than 0 and if wolves don't outnumber sheep
-        if fromBank.sheep > 0 and fromBank.wolves > 0 and fromBank.sheep - 1 < fromBank.wolves - 1:
-            # Checking if sheep greater than or equal to wolves on other side of bank
-            if toBank.sheep + 1 >= toBank.wolves + 1:
-                # Checking if previous state has been seen before
-                for states in self.previousStates:
-                    # Checking left bank sheeps, wolves
-                    if 
-
-        # 2) 1 sheep, 0 wolf
-        # 3) 0 sheep, 1 wolf
+    def printStates(self, statesList):
+        for state in statesList:
+            print(state.getState())
 
 test_agent = SemanticNetsAgent()
 print(test_agent.solve(1, 1))
