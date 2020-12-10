911bdeb1686a79ac645b42316f142b022c55bfb0
tebaker <taalonn@gmail.com>
2020-12-09 18:09:59 -0800

    Added more. Still having problems with an infinite loop. Will fix later

--- ParticipationProjects/mini-p1/SemanticNetsAgent.py
+++ ParticipationProjects/mini-p1/SemanticNetsAgent.py
@@ -42,8 +42,10 @@ class SemanticNetsAgent:
         # Holding list of tuples required to solve the problem
         self.listOfTuples = []
 
-        # Holding list of all previous states
-        self.listOfPreviousStates = []
+        self.previouslyEncounteredStates = []
+
+        # Holding the generations
+        self.unevaluatedStates = []
 
         # If the game is still in progress, inProgress is True
         self.inProgress = True
@@ -56,106 +58,136 @@ class SemanticNetsAgent:
         rightBank = Bank(0, 0)
         currentState = State(leftBank, rightBank, "left")
         
-        '''print("Current State:")
-        currentState.print()'''
+        print("Current State:")
+        currentState.print()
 
+        print("++++++++++++++++++++++++++++++++++++++++")
+        print("++++++++++++++++++++++++++++++++++++++++")
 
-        # Adding initial state to list of previous states
-        self.listOfPreviousStates.append(currentState)
+        self.unevaluatedStates.append(currentState)
+        self.previouslyEncounteredStates.append(currentState)
 
-        while self.inProgress:
-            self.generateAndTest(currentState)
-            break
+        self.generateAndTest(currentState)
 
     # Generate will populate a list of all possible next-states the game
     # progress to regardless of legality or not
     def generateAndTest(self, currentState):
-        # Holding the generations
-        generatedStates = []
+        while self.inProgress:
+            for i in self.unevaluatedStates:
+                i.print()
 
-        # Getting left bank sheep, wolves
-        lBankSheep = currentState.leftBank.getSheep()
-        lBankWolves = currentState.leftBank.getWolves()
+            print("========================================")
 
-        # Getting right bank sheep, wolves
-        rBankSheep = currentState.rightBank.getSheep()
-        rBankWolves = currentState.rightBank.getWolves()
+            # Getting left bank sheep, wolves
+            lBankSheep = currentState.leftBank.getSheep()
+            lBankWolves = currentState.leftBank.getWolves()
 
-        # Getting current boat location
-        currBoatLoc = currentState.getBoatLoc()
+            # Getting right bank sheep, wolves
+            rBankSheep = currentState.rightBank.getSheep()
+            rBankWolves = currentState.rightBank.getWolves()
 
-        print("Current: ", (lBankSheep, lBankWolves), (rBankSheep, rBankWolves), currBoatLoc)
+            # Getting current boat location
+            currBoatLoc = currentState.getBoatLoc()
 
-        if currBoatLoc == "left":
-            # Possible generations for LEFT bank:
-            # 1 sheep, 1 wolf
-            if lBankSheep - 1 >= 0 and lBankWolves - 1 >= 0:
-                holdState = State(Bank(lBankSheep-1, lBankWolves-1), Bank(rBankSheep+1, rBankWolves+1), "right")
-                if self.test(holdState, "left"):
-                    generatedStates.append(holdState)
-            # 1 sheep, 0 wolves
-            if lBankSheep -1 >= 0:
-                holdState = State(Bank(lBankSheep-1, lBankWolves), Bank(rBankSheep+1, rBankWolves), "right")
-                if self.test(holdState, "left"):
-                    generatedStates.append(holdState)
-            # 0 sheep, 1 wolf
-            if lBankWolves -1 >= 0:
-                holdState = State(Bank(lBankSheep, lBankWolves-1), Bank(rBankSheep, rBankWolves+1), "right")
-                if self.test(holdState, "left"):
-                    generatedStates.append(holdState)
-        else:
-            # Possible generations for RIGHT bank:
-            # 1 sheep, 1 wolf
-            if rBankSheep - 1 >= 0 and rBankWolves - 1 >= 0:
-                holdState = State(Bank(lBankSheep+1, lBankWolves+1), Bank(rBankSheep-1, rBankWolves-1), "left")
-                if self.test(holdState, "right"):
-                    generatedStates.append(holdState)
-            # 1 sheep, 0 wolves
-            if rBankSheep -1 >= 0:
-                holdState = State(Bank(lBankSheep+1, lBankWolves), Bank(rBankSheep-1, rBankWolves), "left")
-                if self.test(holdState, "right"):
-                    generatedStates.append(holdState)
-            # 0 sheep, 1 wolf
-            if rBankWolves -1 >= 0:
-                holdState = State(Bank(lBankSheep, lBankWolves+1), Bank(rBankSheep, rBankWolves-1), "left")
-                if self.test(holdState, "right"):
-                    generatedStates.append(holdState)
-
-        print("Legal States: ")
-        for state in generatedStates:
-            state.print()
+            if currentState.leftBank.getSheep() == 0 and currentState.leftBank.getWolves() == 0:
+                self.inProgress = False
+                print("FINISHED!")
+                print(self.listOfTuples)
+            
+            elif currBoatLoc == "left":
+                # Possible generations for LEFT bank:
+                # 1 sheep, 1 wolf
+                if lBankSheep - 1 >= 0 and lBankWolves - 1 >= 0:
+                    holdState = State(Bank(lBankSheep-1, lBankWolves-1), Bank(rBankSheep+1, rBankWolves+1), "right")
+                    if self.test(holdState, "left"):
+                        self.unevaluatedStates.append(holdState)
+                        self.listOfTuples.append((1, 1))
+                        self.previouslyEncounteredStates.append(holdState)
+                # 1 sheep, 0 wolves
+                if lBankSheep -1 >= 0:
+                    holdState = State(Bank(lBankSheep-1, lBankWolves), Bank(rBankSheep+1, rBankWolves), "right")
+                    if self.test(holdState, "left"):
+                        self.unevaluatedStates.append(holdState)
+                        self.listOfTuples.append((1, 0))
+                        self.previouslyEncounteredStates.append(holdState)
+                # 0 sheep, 1 wolf
+                if lBankWolves -1 >= 0:
+                    holdState = State(Bank(lBankSheep, lBankWolves-1), Bank(rBankSheep, rBankWolves+1), "right")
+                    if self.test(holdState, "left"):
+                        self.unevaluatedStates.append(holdState)
+                        self.listOfTuples.append((0, 1))
+                        self.previouslyEncounteredStates.append(holdState)
+            else:
+                # Possible generations for RIGHT bank:
+                # 1 sheep, 1 wolf
+                if rBankSheep - 1 >= 0 and rBankWolves - 1 >= 0:
+                    holdState = State(Bank(lBankSheep+1, lBankWolves+1), Bank(rBankSheep-1, rBankWolves-1), "left")
+                    if self.test(holdState, "right"):
+                        self.unevaluatedStates.append(holdState)
+                        self.listOfTuples.append((1, 1))
+                        self.previouslyEncounteredStates.append(holdState)
+                # 1 sheep, 0 wolves
+                if rBankSheep -1 >= 0:
+                    holdState = State(Bank(lBankSheep+1, lBankWolves), Bank(rBankSheep-1, rBankWolves), "left")
+                    if self.test(holdState, "right"):
+                        self.unevaluatedStates.append(holdState)
+                        self.listOfTuples.append((1, 0))
+                        self.previouslyEncounteredStates.append(holdState)
+                # 0 sheep, 1 wolf
+                if rBankWolves -1 >= 0:
+                    holdState = State(Bank(lBankSheep, lBankWolves+1), Bank(rBankSheep, rBankWolves-1), "left")
+                    if self.test(holdState, "right"):
+                        self.unevaluatedStates.append(holdState)
+                        self.listOfTuples.append((0, 1))
+                        self.previouslyEncounteredStates.append(holdState)
+                        
 
     # Test will take in list of generated possible next-states and evaluate
     # them for legality, discarding the illegal next-states
-    def test(self, generatedState, fromLoc):
+    def test(self, checkState, fromLoc):
+        lSheep  = checkState.getLBank().getSheep()
+        lWolves = checkState.getLBank().getWolves()
 
-        lSheep  = generatedState.getLBank().getSheep()
-        lWolves = generatedState.getLBank().getWolves()
+        rSheep  = checkState.getRBank().getSheep()
+        rWolves = checkState.getRBank().getWolves()
 
-        rSheep  = generatedState.getRBank().getSheep()
-        rWolves = generatedState.getRBank().getWolves()
+        checkPass = True
 
         # The following will make a state illegal:
         # If the boat took only one animal to an empty side
         if fromLoc == "left":
             if rSheep + rWolves == 1:
-                return False
+                checkPass = False
         else:
             if lSheep + lWolves == 1:
-                return False
+                checkPass = False
         
         # If the number of wolves outweight the number of sheeps on either side
         if lWolves > lSheep:
-            return False
+            checkPass = False
         elif rWolves > rSheep:
-            return False
-        else:
+            checkPass = False
+        
+        # If the check state has been incountered before (contained within generatedStates list),
+        # it is an illegal state
+        if checkPass:
+            for state in self.previouslyEncounteredStates:
+                if state.getLBank().getSheep() == lSheep:
+                    if state.getLBank().getWolves() == lWolves:
+                        if state.getRBank().getSheep() == rSheep:
+                            if state.getRBank().getWolves() == rWolves:
+                                # State encountered before; illegal
+                                checkPass = False
+
+        if checkPass:
             return True
+        else:
+            return False
 
 
 test_agent = SemanticNetsAgent()
-print(test_agent.solve(1, 1))
-# print(test_agent.solve(2, 2))
+# print(test_agent.solve(1, 1))
+print(test_agent.solve(2, 2))
 # print(test_agent.solve(3, 3))
 # print(test_agent.solve(5, 3))
 # print(test_agent.solve(6, 3))
